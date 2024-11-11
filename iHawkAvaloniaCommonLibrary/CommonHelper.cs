4c097209179b7fba0e9efc8b9589c2d1f1b8d182
DavidHawkes <db300@163.com>
2024-11-11 16:57:51 +0800

    创建项目及初始代码

---
+++ iHawkAvaloniaCommonLibrary/CommonHelper.cs
@@ -0,0 +1,41 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+
+namespace iHawkAvaloniaCommonLibrary
+{
+    public static class CommonHelper
+    {
+        public static async Task<IReadOnlyList<IStorageFile>?> OpenPdfFileAsync(Visual visual)
+        {
+            if (TopLevel.GetTopLevel(visual) is not TopLevel topLevel) return null;
+            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                AllowMultiple = true,
+                FileTypeFilter = new List<FilePickerFileType>
+                {
+                    new("pdf文件") { Patterns = new List<string> { "*.pdf" } },
+                    new("所有文件") { Patterns = new List<string> { "*.*" } }
+                }
+            });
+            return files;
+        }
+
+        public static async Task<IReadOnlyList<IStorageFile>?> OpenFontFileAsync(Visual? visual)
+        {
+            if (TopLevel.GetTopLevel(visual) is not TopLevel topLevel) return null;
+            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                AllowMultiple = true,
+                FileTypeFilter = new List<FilePickerFileType>
+                {
+                    new("字库文件") { Patterns = new List<string> { "*.ttf", "*.otf" } },
+                    new("ttf文件") { Patterns = new List<string> { "*.ttf" } },
+                    new("otf文件") { Patterns = new List<string> { "*.otf" } },
+                    new("所有文件") { Patterns = new List<string> { "*.*" } }
+                }
+            });
+            return files;
+        }
+    }
+}
