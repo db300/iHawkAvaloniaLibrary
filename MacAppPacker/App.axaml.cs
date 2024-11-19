d96196e3dbdf968cbaa02201eadc30ee6b025bf5
David Hawkes <db300@163.com>
2024-11-19 13:22:57 +0800

    调整

--- MacAppPacker/App.axaml.cs
+++ MacAppPacker/App.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
@@ -19,6 +20,12 @@ namespace MacAppPacker
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private void NativeMenuAbout_Click(object? sender, System.EventArgs e)
+        {
+            //TODO:执行关于操作
+            Console.WriteLine("About clicked");
         }
     }
 }
